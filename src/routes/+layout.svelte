63535c245eeee335d8d56ce06757d2ea7d298b53
stef <stefthedoggo4@gmail.com>
2022-12-20 21:23:39 +0000

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="content text-gray-100 font-sans">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="bg-gradient-to-b from-gray-900 to-zinc-900 h-screen grid place-content-center">
+	<b>
+		<h1 class="text-4xl underline-offset-2">
+			Welcome to 
+            <p class="underline decoration-sky-500">Confounding Technologies</p>
+		</h1>
+	</b>
+</div>
