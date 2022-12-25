52471cd5c5169287b2c243eded8470753cef1b20
stef <stefthedoggo4@gmail.com>
2022-12-25 18:21:11 +0000

    a

---
+++ src/routes/images/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Confounding Technologies</title>
+	<meta name="author" content="stef#6470" />
+	<meta
+		name="description"
+		content="The Confounding Technologies homepage.<br>Perplexing or Bewildering."
+	/>
+</svelte:head>
+
+<div class="bg-gradient-to-b from-gray-900 to-zinc-900 grid place-content-center">
+	<b>
+		<h1 class="text-4xl underline-offset-2 p-8">
+			Welcome to
+			<p class="underline decoration-sky-500">Image Recognition</p>
+		</h1>
+	</b>
+</div>
+<div class="content">
+	<section class="p-8">
+		<div class="flex flex-row">
+			<div class="bg-gray-100 w-2 h-10" />
+			<div class="pl-4">
+				<h3 class="text-2xl pb-4 underline decoration-pink-500">
+					Hello! Hi! Here to recognise some images?
+				</h3>
+			</div>
+		</div>
+	</section>
+	<section class="p-8">
+	</section>
+</div>
