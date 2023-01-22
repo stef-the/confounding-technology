35832ccb0375bbe43724c181f208b12850e6d721
stef <stefthedoggo4@gmail.com>
2023-01-22 21:09:43 +0000

    add viewport

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
 </script>
-
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</svelte:head>
 <div class="content text-gray-100 bg-zinc-900 font-sans">
 	<slot />
 </div>
